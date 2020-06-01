d87839a5672bd12b6d547211b9efc662b56b5c16
Robert Stefaniak <01138659@pw.edu.pl>
2020-06-01 18:53:01 +0200

    Add project files.

---
+++ ConsoleApp5_Obiekty/Czlowiek.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5_Obiekty
+{
+    class Czlowiek
+    {
+
+        public string imie;
+        public string nazwisko;
+
+        public Czlowiek(string imieKonstruktora = "BRAK", string nazwiskoKonstruktora = "BRAK")
+        {
+            imie = imieKonstruktora;
+            nazwisko = nazwiskoKonstruktora;
+        }
+        public void PrzedstawSie()
+        {
+            Console.WriteLine("Nazywam sie " + imie + " " + nazwisko);
+        }
+
+        ~Czlowiek()
+        {
+            Console.WriteLine("Jestem destruktorem, ktory tutaj posprzata.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp5_Obiekty/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5_Obiekty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Czlowiek osoba1 = new Czlowiek("Alicja","Strugala");
+            osoba1.PrzedstawSie();
+
+            Console.ReadKey();
+        }
+    }
+
+}
